2de1ce73a0a8e736ed7a5f03675a50612625bcf5
div-seungha <sh.kim.x928@gmail.com>
2025-03-31 19:44:59 +0900

    Add index page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,11 +2,27 @@ import type { MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Muzlinkverse" },
+    {
+      name: "description",
+      content: "Share the music you listening with us :)",
+    },
   ];
 };
 
 export default function Index() {
-  return <div className="flex h-screen items-center justify-center"></div>;
+  return (
+    <div className="index-container">
+      <div>
+        <img
+          src="/under-construction.webp"
+          alt="영차 영차... 아직 작업 중인 사이트입니다."
+          width="20%"
+        />
+        <h1>Under Construction...</h1>
+        <p>현재 작업 중인 웹 사이트입니다.</p>
+        <p>문의: serendipity@beonanotherplanet.com</p>
+      </div>
+    </div>
+  );
 }
