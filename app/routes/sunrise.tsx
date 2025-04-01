c7cfa684718021d77b8c5db20482369b8617c204
div-seungha <sh.kim.x928@gmail.com>
2025-04-01 14:56:01 +0900

    Fix button content alignment

--- app/routes/sunrise.tsx
+++ app/routes/sunrise.tsx
@@ -64,49 +64,49 @@ export default function Index() {
           <a href={youtubeMusic} target="_blank" rel="noreferrer">
             <button className="youtube-music">
               <SiYoutubemusic fontSize={28} />
-              Youtube Music
+              <span className="button-link-name">Youtube Music</span>
             </button>
           </a>
           <a href={youtubeLink} target="_blank" rel="noreferrer">
             <button className="youtube">
               <FaYoutube fontSize={30} />
-              Youtube
+              <span className="button-link-name">Youtube</span>
             </button>
           </a>
           <a href={melon} target="_blank" rel="noreferrer">
             <button className="melon">
               <PiPlayCircleBold fontSize={30} />
-              Melon
+              <span className="button-link-name">Melon</span>
             </button>
           </a>
           <a href={apple} target="_blank" rel="noreferrer">
             <button className="apple">
               <SiApplemusic fontSize={28} />
-              Apple Music
+              <span className="button-link-name">Apple Music</span>
             </button>
           </a>
           <a href={spotify} target="_blank" rel="noreferrer">
             <button className="spotify">
               <FaSpotify fontSize={30} />
-              Spotify
+              <span className="button-link-name">Spotify</span>
             </button>
           </a>
           <a href={bugs} target="_blank" rel="noreferrer">
             <button className="bugs">
               <TbMusicShare fontSize={30} />
-              Bugs
+              <span className="button-link-name">Bugs</span>
             </button>
           </a>
           <a href={naver} target="_blank" rel="noreferrer">
             <button className="naver">
               <FaCirclePlay fontSize={26} />
-              Naver Vibe
+              <span className="button-link-name">Naver Vibe</span>
             </button>
           </a>
           <a href={flo} target="_blank" rel="noreferrer">
             <button className="flo">
               <RiMusicAiFill fontSize={26} />
-              FLO
+              <span className="button-link-name">FLO</span>
             </button>
           </a>
 
